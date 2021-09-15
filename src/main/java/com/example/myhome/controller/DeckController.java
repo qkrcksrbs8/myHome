d40f74ddcc9adc292d8b42fadb1565dd9f7dd93d
ParkChanGyun <qkrcksrbs8@naver.com>
2021-09-16 00:10:52 +0900

    덱 리스트 수정
    덱 리스트 수정

---
+++ src/main/java/com/example/myhome/controller/DeckController.java
@@ -0,0 +1,26 @@
+package com.example.myhome.controller;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/deck")
+public class DeckController {
+
+    @GetMapping("/deckList")
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable,
+                       @RequestParam(required = false, defaultValue = "") String searchText) {
+//
+//        model.addAttribute("startPage"  , startPage);
+//        model.addAttribute("endPage"    , endPage);
+//        model.addAttribute("boards"     , boards);
+        return "deck/deckList";
+    }
+
+
+}

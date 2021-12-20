bdf2b89ee58e71e88891cbfb1df4ff46f13a0ff2
changyun <qkrcksrbs8@naver.com>
2021-12-20 21:59:35 +0900

    로그인 세션 유지

--- src/main/java/com/example/myhome/config/WebSecurityConfig.java
+++ src/main/java/com/example/myhome/config/WebSecurityConfig.java
@@ -12,7 +12,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -52,9 +51,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .csrf().disable()//csrf() 삭제해야함
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and()
+//            .sessionManagement()
+//            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//            .and()
             .authorizeRequests()
             .antMatchers("/", "/account/register", "/css/**","/api/**").permitAll()
             .anyRequest().permitAll()

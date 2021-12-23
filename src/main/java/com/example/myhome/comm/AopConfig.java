528a0a1e72d80d7c970b93445aafeb5f258ab0b7
changyun <qkrcksrbs8@naver.com>
2021-12-23 19:30:06 +0900

    LogUtil 생성

--- src/main/java/com/example/myhome/comm/AopConfig.java
+++ src/main/java/com/example/myhome/comm/AopConfig.java
@@ -1,5 +1,6 @@
 package com.example.myhome.comm;
 
+import com.example.myhome.util.LogUtil;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
@@ -10,9 +11,6 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 @Aspect
 @Component
@@ -29,7 +27,7 @@ public class AopConfig {
         request.getParameterMap();
         logger.info("SESS_GUID = {}, ===================START===================", sessGuid);
         logger.info("SESS_GUID = {}, @Around : {} ", sessGuid, type);
-        logger.info("SESS_GUID = {}, @Around : {}, param : {} ", sessGuid, type, mapToStr(request.getParameterMap()));
+        logger.info("SESS_GUID = {}, @Around : {}, param : {} ", sessGuid, type, LogUtil.mapToStr(request.getParameterMap()));
         return joinPoint.proceed();
     }
 
@@ -39,9 +37,9 @@ public class AopConfig {
         Object[] args = joinPoint.getArgs();
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
         if (args.length == 0 || args[0] instanceof HttpServletRequest){
-            logger.info("SESS_GUID = {}, @Before : {}, param : {}", sessGuid, type, mapToStr(request.getParameterMap()));
+            logger.info("SESS_GUID = {}, @Before : {}, param : {}", sessGuid, type, LogUtil.mapToStr(request.getParameterMap()));
         }else{
-            logger.info("SESS_GUID = {}, @Before : {}, param : {}", sessGuid, type, setParamParse(args[0].toString(), "="));
+            logger.info("SESS_GUID = {}, @Before : {}, param : {}", sessGuid, type, LogUtil.setParamParse(args[0].toString(), "="));
         }
     }
 
@@ -57,84 +55,5 @@ public class AopConfig {
         logger.info("SESS_GUID = {}, ===================E N D===================", sessGuid);
     }
 
-    private boolean isNull(String str) {
-        return (str == null && str == "");
-    }
 
-    public String mapToStr(Map<String, String[]> map){
-        return (map == null || map.keySet().size() == 0)? "{null}":appendMapToStr(map);
-    }
-
-    private String appendMapToStr(Map<String, String[]> map){
-        StringBuffer sb = new StringBuffer();
-        sb.append("{");
-        for (String key : map.keySet()) sb.append(strType(map.get(key), key)).append(", ");
-        sb.append("}");
-        return sb.toString().replace(", }", "}");
-    }
-
-    private String strType (Object val, String key) {
-        boolean chkKey = false;
-        if (val == null) return key + "null";
-        if (val instanceof String) return keyVal(key, val, chkKey);
-        if (val instanceof String[]){
-            String[] arr = (String[]) val;
-            if(arr.length == 1) return keyVal(key, arr[0], chkKey);
-            StringBuilder sb = new StringBuilder();
-            Arrays.stream(arr).forEach(a -> sb.append(keyVal(key, a, chkKey)));
-        }
-        return keyVal(key, val, chkKey);
-    }
-
-    private String keyVal(String key, Object obj, boolean chkKey) {
-        return (chkKey)? key+":"+obj.toString().replaceAll(".*", "****") : key+":"+obj;
-    }
-
-    public String setParamParse(String str, String gubun) {
-        Map<String, String> map = new HashMap<String, String>();
-        if (isNull(str)) return null;
-        if (str.contains("{")) return str;
-        str = str
-                .replace("{", "")
-                .replace("}", "");
-        String[] array = str.split(",");
-
-        for (int i = 0; i < array.length; i++) {
-            String[] param = array[i].split(gubun);
-            if (param.length > 1) {
-                map.put(param[0], param[1]);
-                continue;
-            }
-            map.put(param[0], "");
-        }
-        StringBuilder sb = new StringBuilder();
-        sb.append("{");
-        for (String key : map.keySet()) {
-            Object val = map.get(key);
-            key = key.trim();
-            boolean chkKey = false;
-            sb.append(loopVal(key, val, chkKey, gubun));
-            sb.append(keyVal2(key, val, chkKey, gubun));
-            sb.append(", ");
-        }
-        sb.append("}");
-        return sb.toString().replace(", }", "}");
-    }
-
-    private String keyVal2(String key, Object obj, boolean chkKey, String gubun) {
-        return (chkKey)? key+gubun+obj.toString().replaceAll(".*", "****") : key+gubun+obj;
-    }
-
-    private String loopVal(String key, Object val, boolean chkKey, String gubun){
-        if (val == null) return key + "null";
-        if (val instanceof String) return keyVal2(key, val, chkKey, gubun);
-        if (val instanceof String[]) {
-            String[] arr = (String[]) val;
-            if (arr.length == 1)  return keyVal2(key, arr[0], chkKey, gubun);
-            StringBuilder sb = new StringBuilder();
-            Arrays.stream(arr).forEach(a -> sb.append(keyVal2(key, a, chkKey, gubun)));
-            return sb.toString();
-        }
-        return null;
-    }
 }

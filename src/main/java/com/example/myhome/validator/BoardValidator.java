9b93f6223b8139ee07ff232a657e7d6325b24468
changyun <qkrcksrbs8@naver.com>
2022-12-08 22:05:48 +0900

    board validator

--- src/main/java/com/example/myhome/validator/BoardValidator.java
+++ src/main/java/com/example/myhome/validator/BoardValidator.java
@@ -17,7 +17,7 @@ public class BoardValidator implements Validator {
     @Override
     public void validate(Object obj, Errors errors) {
         Board b = (Board) obj;
-        if (StringUtils.isEmpty(b.getContent())){
+        if (StringUtils.isEmpty(b.getContent())) {
             errors.rejectValue("content", "key", "내용을 입력하세요.");
         }
     }

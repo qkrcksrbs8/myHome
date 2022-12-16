bed3b9bdfd915d595e4a6f25ea8883aeb88fe9ec
changyun <qkrcksrbs8@naver.com>
2022-12-16 22:39:35 +0900

    User

--- src/main/java/com/example/myhome/model/User.java
+++ src/main/java/com/example/myhome/model/User.java
@@ -27,7 +27,7 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private List<Role> roles = new ArrayList<>();
 
-//    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    //    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonIgnore
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private List<Board> boards = new ArrayList<>();
